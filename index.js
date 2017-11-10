78223599a6679cde6fb0cc6dfda16641a45b979c
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-11-10 18:56:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,4 +66,7 @@ function placeOrder(cardNumber) {
   if(!cardNumber){
     return console.log("Sorry, we don't have a credit card on file for you.")
   }
+  else{
+    return console.log(`Your total cost is ${total()}, which will be charged to the card ${cardnumber}.`)
+  }
 }
